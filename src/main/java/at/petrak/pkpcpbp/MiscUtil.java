4a5bf09ebd2a3b2d225e6f2f2fbe5fe5ba566034
object-Object <object@objectobject.ca>
2026-08-26 02:18:46 -0400

    Maybe fix the execs

--- src/main/java/at/petrak/pkpcpbp/MiscUtil.java
+++ src/main/java/at/petrak/pkpcpbp/MiscUtil.java
@@ -2,44 +2,33 @@ package at.petrak.pkpcpbp;
 
 import org.gradle.api.Project;
 
-import java.io.ByteArrayOutputStream;
 import java.util.regex.Pattern;
 
 public class MiscUtil {
   /** Gets a list of commit messages since the previous commit built, prepending {@code - } to the start of each. */
   public static String getRawGitChangelogList(Project project) {
-    var stdout = new ByteArrayOutputStream();
     var gitHash = System.getenv("GIT_COMMIT");
     var gitPrevHash = System.getenv("GIT_PREVIOUS_COMMIT");
     var travisRange = System.getenv("TRAVIS_COMMIT_RANGE");
 
     if (gitHash != null && gitPrevHash != null) {
-      project.getProviders().exec(spec -> {
-        spec.commandLine("git", "log", "--pretty=tformat:- %s", gitPrevHash + "..." + gitHash);
-        spec.setStandardOutput(stdout);
-      });
+      return project.getProviders().exec(spec ->
+        spec.commandLine("git", "log", "--pretty=tformat:- %s", gitPrevHash + "..." + gitHash)
+      ).getStandardOutput().getAsText().get(); // hack
     } else if (travisRange != null) {
-      project.getProviders().exec(spec -> {
-        spec.commandLine("git", "log", "--pretty=tformat:- %s", travisRange);
-        spec.setStandardOutput(stdout);
-      });
+      return project.getProviders().exec(spec ->
+        spec.commandLine("git", "log", "--pretty=tformat:- %s", travisRange)
+      ).getStandardOutput().getAsText().get();
     } else {
       return "";
     }
-
-    return stdout.toString();
   }
 
   /** Gets the most recent commit message as-is. */
   public static String getMostRecentPush(Project project) {
-    var stdout = new ByteArrayOutputStream();
-
-    project.getProviders().exec(spec -> {
-      spec.commandLine("git", "log", "--pretty=tformat:%s", "HEAD~..HEAD");
-      spec.setStandardOutput(stdout);
-    });
-
-    return stdout.toString();
+    return project.getProviders().exec(spec ->
+        spec.commandLine("git", "log", "--pretty=tformat:%s", "HEAD~..HEAD")
+    ).getStandardOutput().getAsText().get();
   }
 
   /**
